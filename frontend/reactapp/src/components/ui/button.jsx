5979b624059cd4c013796468d7b1c94605e2366f
tomoakisakizono <123532464+tomoakisakizono@users.noreply.github.com>
2025-02-07 13:49:17 +0900

    Initial commit

--- frontend/reactapp/src/components/ui/button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Button = ({ children, ...props }) => (
-  <button {...props} className="p-2 bg-blue-500 text-white rounded">
-    {children}
-  </button>
-);
